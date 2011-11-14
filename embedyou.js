ad374b6e31877e19bf7a7d281d21b9362657c951
Neill Russell <n@enru.co.uk>
2011-11-14 22:29:16 +0000

    devel branch. adding other you tube urls.

--- embedyou.js
+++ embedyou.js
@@ -1,5 +1,12 @@
 var EmbedYou = {
-	reObj: /http(s)?:\/\/www\.youtube\.com\/watch\?v=[a-z0-9]+/i
+	regex: [
+        /http(s)?:\/\/(www\.)?\youtube\.com\/watch\?.*v=([a-z0-9]+)/i
+        /* ignore embed urls
+        ,/http(s)?:\/\/(www\.)?\youtube\.com\/v/([a-z0-9]+)/i
+        ,/http(s)?:\/\/(www\.)?\youtube\.com\/embed/([a-z0-9]+)/i
+        */
+        ,/http(s)?:\/\/(www\.)?\youtu\.be\/([a-z0-9]+)/i
+    ]
 	,initCSS: function(url) {
 		var head = document.getElementsByTagName("head")[0];
         var css = document.createElement("link");
@@ -10,15 +17,17 @@ var EmbedYou = {
 	,init: function() {
 		this.initCSS();
 		var anchors = document.getElementsByTagName('a');
-		for(var i = 0; i < anchors.length; i++) {
+		for(var i=0; i < anchors.length; i++) {
 			var a = anchors[i];
-			if(this.reObj.test(a.href)) this.handle(a)
+            for(var j=0; j<this.regex.length; j++) {
+                var matches = a.href.match(this.regex[j]);
+                if(matches) this.handle(a, "embedyou_"+matches[3]+"_"+i)
+            }
 		}
 	}
-	,handle: function(anchor) {
-		this.append(anchor.href);
-		var id = anchor.href.match(/v=[0-9a-z]+/i)[0].split('=')[1];
-		anchor.id = 'embedyou_'+id
+	,handle: function(anchor, id) {
+		this.append(anchor.href, id);
+		anchor.id = id
 	}
 	,load: function(id, code) {
 		var a = document.getElementById(id);
@@ -28,12 +37,12 @@ var EmbedYou = {
 		a.style.display = 'none';
 		a.parentNode.insertBefore(html, a);
 	}
-	,append: function(url) {
+	,append: function(url, id) {
 		var head = document.getElementsByTagName("head")[0];
-        var script  = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "http://www.enru.co.uk/embedyou/v1/embedyou.php?url="+url;
-        head.appendChild(script)
+        	var script  = document.createElement("script");
+        	script.type = "text/javascript";
+        	script.src = "http://www.enru.co.uk/embedyou/d/embedyou.php?url="+encodeURIComponent(url)+"&id="+encodeURIComponent(id);
+        	head.appendChild(script)
 	}
 }
 EmbedYou.init();
--- embedyou.php
+++ embedyou.php
@@ -1,16 +1,40 @@
 <?php
 
+function url_ok($url) {
+
+    $regexs = array(
+            "/http(s)?:\/\/(www\.)?\youtube\.com\/watch\?.*v=([a-z0-9]+)/i"
+            ,"/http(s)?:\/\/(www\.)?\youtu\.be\/([a-z0-9]+)/i"
+            /* ignore embed urls
+            ,"/http(s)?:\/\/(www\.)?\youtube\.com\/v/([a-z0-9]+)/i"
+            ,"/http(s)?:\/\/(www\.)?\youtube\.com\/embed/([a-z0-9]+)/i"
+            */
+    );
+
+    foreach($regexs as $re) {
+        $matches = array();
+        echo $re;
+        echo "<br/>\n";
+	    if(preg_match($re, $url, $matches)) return true;
+    }
+
+    return false;
+}
+
 $js='';
 if(isset($_REQUEST['url'])) {
 	$url = parse_url($_REQUEST['url']);
-	if(preg_match("/youtube.com$/", $url['host'])) {
+	//if(url_ok($url['host'])) {
+    if(preg_match("/(youtube.com|youtu.be)$/", $url['host'])) {
 		parse_str($url['query'], $qs);
 		if(isset($qs['v'])) {
-			$req = 'http://www.youtube.com/oembed?url='.urlencode($url['scheme'].'://'.$url['host'].'/watch?v='.$qs['v']);
+            $id = 'embedyou_'.$qs['v'];
+            if(isset($_GET['id']) && preg_match("/^[a-zA-Z0-9_]+$/", $_GET['id'])) $id = $_GET['id'];
+			$req = 'http://www.youtube.com/oembed?url='.urlencode($_GET['url']);
 			$res = file_get_contents($req);
 			if($res) {
 				$obj = 	json_decode($res);
-				$js = "EmbedYou.load('embedyou_".$qs['v']."', '".$obj->html."')";
+				$js = "EmbedYou.load('".$id."', '".$obj->html."')";
 			}
 		}
 	}
